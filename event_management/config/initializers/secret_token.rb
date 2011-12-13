ad45ab0d8208a7de320069fd48470201e4a540e6
nobinobiru <haradashinya@gmail.com>
2011-12-13 20:02:42 +0900

    remove duplicate project

--- event_management/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-EventManagement::Application.config.secret_token = '392f160bfc766f07d76ec5439c6adb3f03adb3bc3f2354d1b1f96a8269475254b585b5745e41183be2c865c90cd16258160ddc1f08cec4d97865d87098220a8a'
