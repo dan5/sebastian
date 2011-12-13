6ed9978002f37d009726d8f5e40b38599d0476a2
nobinobiru <haradashinya@gmail.com>
2011-12-13 20:23:08 +0900

    initialize

--- event-manage/config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,"RAyt3kuSeQUbntImmfXlg", "WGQYGOiIeZP2n7NcmyhZa9hdQg3XUD5Z54vIK8RAA"
-  # provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-  # provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
-  provider :identity, on_failed_registration: lambda { |env|
-    IdentitiesController.action(:new).call(env)
-  }
-end
--- event-manage/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Auth::Application.routes.draw do
-  root to: "sessions#new"
-  match "/auth/:provider/callback", to: "sessions#create"
-  match "/auth/failure", to: "sessions#failure"
-  match "/logout", to: "sessions#destroy", :as => "logout"
-  resources :identities
-end
